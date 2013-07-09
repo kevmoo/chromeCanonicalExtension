42ef45156f1ea950df58945da10462921ca90f1a
Kevin Moore <kevin@thinkpixellab.com>
2013-07-09 19:37:01 -0400

    moving to manifest version 2

---
+++ background.js
@@ -0,0 +1,40 @@
+
+var canonical_urls = {};
+
+chrome.extension.onRequest.addListener(function(request, sender, callback) {
+  if(request.action){
+    if(request.action == 'log'){
+      console.log(request.text);
+    }
+    else if(request.action == 'error'){
+      console.error(request.text);
+    }
+    else if(request.action == 'setCanonical'){
+      canonical_urls[sender.tab.id] = request.url;
+      chrome.pageAction.setTitle(
+        {
+          tabId: sender.tab.id,
+          title: "Canonical url: " + request.url
+        }
+      );
+      chrome.pageAction.show(sender.tab.id);
+    }
+  }
+});
+
+// tab is going away, remove the canonical data for it
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  delete canonical_urls[tabId];
+});
+
+// address bar icon clicked, navigate the tap to the canonical url
+chrome.pageAction.onClicked.addListener( function(tab) {
+  chrome.tabs.update(tab.id, {url: canonical_urls[tab.id]});
+});
+
+chrome.tabs.onUpdated.addListener( function(tabId, changeInfo, tab) {
+  // ensure that the url data lives for the life of the page, not the tab
+  if(changeInfo.status == "loading"){
+    delete canonical_urls[tabId];
+  }
+});
